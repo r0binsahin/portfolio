9d97d483b38656a513b8b186dbd683dd08e7dfc8
r0binSahin <robin.sahin88@gmail.com>
2024-04-12 15:30:50 +0200

    🚀(feat): footer added

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footerWrapper">
+      <div className="contactLinks"></div>
+      <div className="fullName">
+        <p>robin sahin</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
