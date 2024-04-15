027c68e7d1381ec227273dcf273385a90732337b
r0binSahin <robin.sahin88@gmail.com>
2024-04-15 10:20:37 +0200

    🎨(style): footer styled

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,16 @@ import "./footer.scss";
 const Footer = () => {
   return (
     <div className="footerWrapper">
-      <div className="contactLinks"></div>
+      <div className="contact">
+        <div className="contact__links">
+          <p>contact </p>
+          <p> links</p>
+          <p>etc</p>
+        </div>
+        <div className="contact__copy">
+          <p>&copy; 2024</p>
+        </div>
+      </div>
       <div className="fullName">
         <p>robin sahin</p>
       </div>
