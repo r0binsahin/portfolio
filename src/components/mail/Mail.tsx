c5ee4cec9ab5a9e1098772f99299a4f2788497bf
r0binSahin <robin.sahin88@gmail.com>
2024-04-15 15:36:33 +0200

    (🚀feat): btn added

--- src/components/mail/Mail.tsx
+++ src/components/mail/Mail.tsx
@@ -7,192 +7,179 @@ const Mail = () => {
     <div className="mailContainer">
       <h1>contact me</h1>
       <div className="inputs">
-        <TextField
-          /*  variant="standard"
-          required
-          fullWidth
-          label="Name"
-          type="Text"
-          InputLabelProps={{
-            style: {
-              color: "var(--body-color)",
-              borderColor: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
-            },
-          }}
-         
-          sx={{
-            "& .MuiInput-root": {
-              "&:before": {
-                borderColor: "var(--body-color)",
-              },
-              // Border on focus
-              "&:after": {
-                borderColor: "var(--body-color)",
-                borderWidth: "1px",
-              },
-              ":hover(.Mui-hover)": {
+        <form action="submit">
+          <TextField
+            label="Name"
+            variant="standard"
+            required
+            fullWidth
+            sx={{
+              "& .MuiInput-root": {
+                color: "var(--body-color)",
+                fontFamily: " var(--font-jakart)",
+
+                // Bottom border
                 "&:before": {
                   borderColor: "var(--body-color)",
-                  borderWidth: "1px",
+                  borderWidth: 1,
                 },
-              },
-            },
-          }} */
-
-          label="Name"
-          variant="standard"
-          required
-          fullWidth
-          sx={{
-            "& .MuiInput-root": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
-
-              // Bottom border
-              "&:before": {
-                borderColor: "var(--body-color)",
-                borderWidth: 1,
-              },
-              // Border on focus
-              "&:after": {
-                borderColor: "var(--font-jakart)",
-                borderWidth: 1,
-              },
-              ":hover:not(.Mui-focused)": {
-                "&:before": {
+                // Border on focus
+                "&:after": {
                   borderColor: "var(--font-jakart)",
                   borderWidth: 1,
                 },
+                ":hover:not(.Mui-focused)": {
+                  "&:before": {
+                    borderColor: "var(--font-jakart)",
+                    borderWidth: 1,
+                  },
+                },
               },
-            },
-            // Label
-            "& .MuiInputLabel-standard": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
-
-              "&.Mui-focused": {
+              // Label
+              "& .MuiInputLabel-standard": {
                 color: "var(--body-color)",
-              },
-            },
-          }}
-        />
-        <TextField
-          variant="standard"
-          required
-          fullWidth
-          label="Email"
-          type="email"
-          sx={{
-            "& .MuiInput-root": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
+                fontFamily: " var(--font-jakart)",
 
-              // Bottom border
-              "&:before": {
-                borderColor: "var(--body-color)",
-                borderWidth: 1,
+                "&.Mui-focused": {
+                  color: "var(--body-color)",
+                },
               },
-              // Border on focus
-              "&:after": {
-                borderColor: "var(--font-jakart)",
-                borderWidth: 1,
+              "& .MuiInput-underline": {
+                paddingTop: "20px",
               },
-              ":hover:not(.Mui-focused)": {
+            }}
+          />
+          <TextField
+            variant="standard"
+            required
+            fullWidth
+            label="Email"
+            type="email"
+            sx={{
+              "& .MuiInput-root": {
+                color: "var(--body-color)",
+                fontFamily: " var(--font-jakart)",
+
+                // Bottom border
                 "&:before": {
+                  borderColor: "var(--body-color)",
+                  borderWidth: 1,
+                },
+                // Border on focus
+                "&:after": {
                   borderColor: "var(--font-jakart)",
                   borderWidth: 1,
                 },
+                ":hover:not(.Mui-focused)": {
+                  "&:before": {
+                    borderColor: "var(--font-jakart)",
+                    borderWidth: 1,
+                  },
+                },
               },
-            },
-            // Label
-            "& .MuiInputLabel-standard": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
-
-              "&.Mui-focused": {
+              // Label
+              "& .MuiInputLabel-standard": {
                 color: "var(--body-color)",
-              },
-            },
-          }}
-        />
-        <TextField
-          variant="standard"
-          label="Phone"
-          fullWidth
-          type="Text"
-          sx={{
-            "& .MuiInput-root": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
+                fontFamily: " var(--font-jakart)",
 
-              // Bottom border
-              "&:before": {
-                borderColor: "var(--body-color)",
-                borderWidth: 1,
+                "&.Mui-focused": {
+                  color: "var(--body-color)",
+                },
               },
-              // Border on focus
-              "&:after": {
-                borderColor: "var(--font-jakart)",
-                borderWidth: 1,
+              "& .MuiInput-underline": {
+                paddingTop: "20px",
               },
-              ":hover:not(.Mui-focused)": {
+            }}
+          />
+          <TextField
+            variant="standard"
+            label="Phone"
+            fullWidth
+            type="Text"
+            sx={{
+              "& .MuiInput-root": {
+                color: "var(--body-color)",
+                fontFamily: " var(--font-jakart)",
+
+                // Bottom border
                 "&:before": {
+                  borderColor: "var(--body-color)",
+                  borderWidth: 1,
+                },
+                // Border on focus
+                "&:after": {
                   borderColor: "var(--font-jakart)",
                   borderWidth: 1,
                 },
+                ":hover:not(.Mui-focused)": {
+                  "&:before": {
+                    borderColor: "var(--font-jakart)",
+                    borderWidth: 1,
+                  },
+                },
               },
-            },
-            // Label
-            "& .MuiInputLabel-standard": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
-
-              "&.Mui-focused": {
+              // Label
+              "& .MuiInputLabel-standard": {
                 color: "var(--body-color)",
-              },
-            },
-          }}
-        />
-        <TextField
-          variant="standard"
-          required
-          label="Message"
-          type="Text"
-          fullWidth
-          sx={{
-            "& .MuiInput-root": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
+                fontFamily: " var(--font-jakart)",
 
-              // Bottom border
-              "&:before": {
-                borderColor: "var(--body-color)",
-                borderWidth: 1,
+                "&.Mui-focused": {
+                  color: "var(--body-color)",
+                },
               },
-              // Border on focus
-              "&:after": {
-                borderColor: "var(--font-jakart)",
-                borderWidth: 1,
+              "& .MuiInput-underline": {
+                paddingTop: "20px",
               },
-              ":hover:not(.Mui-focused)": {
+            }}
+          />
+          <TextField
+            variant="standard"
+            required
+            /*   multiline
+            rows={5} */
+            label="Message"
+            type="Text"
+            fullWidth
+            sx={{
+              "& .MuiInput-root": {
+                color: "var(--body-color)",
+                fontFamily: " var(--font-jakart)",
+
+                // Bottom border
                 "&:before": {
+                  borderColor: "var(--body-color)",
+                  borderWidth: 1,
+                },
+                // Border on focus
+                "&:after": {
                   borderColor: "var(--font-jakart)",
                   borderWidth: 1,
                 },
+                ":hover:not(.Mui-focused)": {
+                  "&:before": {
+                    borderColor: "var(--font-jakart)",
+                    borderWidth: 1,
+                  },
+                },
               },
-            },
-            // Label
-            "& .MuiInputLabel-standard": {
-              color: "var(--body-color)",
-              fontFamily: " var(--font-jakart)",
-
-              "&.Mui-focused": {
+              // Label
+              "& .MuiInputLabel-standard": {
                 color: "var(--body-color)",
+                fontFamily: " var(--font-jakart)",
+
+                "&.Mui-focused": {
+                  color: "var(--body-color)",
+                },
               },
-            },
-          }}
-        />
+
+              "& .MuiInput-underline": {
+                paddingTop: "20px",
+              },
+            }}
+          />
+
+          <button type="submit">Submit </button>
+        </form>
       </div>
     </div>
   );
