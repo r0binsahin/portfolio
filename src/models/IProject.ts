16acee375a342c8f92ddd0b67eebdeda2a55fa50
r0binSahin <robin.sahin88@gmail.com>
2024-04-22 11:17:26 +0200

    (feat): data added

---
+++ src/models/IProject.ts
@@ -0,0 +1,14 @@
+export interface IProject {
+  id: number;
+  title: string;
+  captionText: string;
+  tools: string[];
+  desc: string;
+  github: string;
+  live: string;
+  isLive: boolean;
+  img1: string;
+  img2: string;
+  img3: string;
+  imgExtra: string;
+}
