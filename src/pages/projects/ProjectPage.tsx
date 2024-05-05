5958705413019f577ec49f7428d4cc6620c81e91
r0binsahin <robin.sahin88@gmail.com>
2024-05-05 15:38:31 +0200

    (feat): project images done

--- src/pages/projects/ProjectPage.tsx
+++ src/pages/projects/ProjectPage.tsx
@@ -1,24 +1,24 @@
-import Accordion from "../../components/accordion/Accordion";
+import Accordion from '../../components/accordion/Accordion';
 
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import ProjectTitle from "../../components/projectTitle/ProjectTitle";
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import ProjectTitle from '../../components/projectTitle/ProjectTitle';
 
 const ProjectPage = () => {
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
     const tlpp = gsap.timeline();
 
-    tlpp.from(".projectWrapper", { duration: 2, y: 1000 });
+    tlpp.from('.projectWrapper', { duration: 2, y: 1000 });
 
     gsap.timeline({
       scrollTrigger: {
-        trigger: ".projectInner__header:first-child",
-        start: "top top-=25%",
-        end: "bottom bottom",
-        onEnter: () => gsap.to(".navbar", { y: "-100%", duration: 0.5 }),
-        onLeaveBack: () => gsap.to(".navbar", { y: "0%", duration: 0.5 }),
+        trigger: '.projectInner__header:first-child',
+        start: 'top top-=25%',
+        end: 'bottom bottom',
+        onEnter: () => gsap.to('.navbar', { y: '-100%', duration: 0.5 }),
+        onLeaveBack: () => gsap.to('.navbar', { y: '0%', duration: 0.5 }),
         pin: true,
         preventOverlaps: true,
       },
@@ -27,10 +27,13 @@ const ProjectPage = () => {
 
   return (
     <>
-      <div className="projectWrapper">
+      <div className='projectWrapper'>
         <ProjectTitle />
       </div>
       <Accordion />
+      <div
+        style={{ width: '100%', height: '450px', background: '#ffb800' }}
+      ></div>
     </>
   );
 };
