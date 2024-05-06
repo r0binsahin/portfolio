eb6c5ca563cae8c283f07f81703a544b14a93bd5
r0binsahin <robin.sahin88@gmail.com>
2024-05-06 20:11:51 +0200

    (feat): developer credited in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,38 +1,39 @@
-import "./footer.scss";
-import { Link } from "react-router-dom";
+import './footer.scss';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="footerWrapper">
-      <div className="contact">
-        <div className="contact__links">
-          <Link to={"/contact"} className="contact__links--link">
+    <div className='footerWrapper'>
+      <div className='contact'>
+        <div className='contact__links'>
+          <Link to={'/contact'} className='contact__links--link'>
             <p>contact </p>
           </Link>
 
           <a
-            href="https://www.linkedin.com/in/robin-sahin-15404a24b/"
-            className="contact__links--link"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.linkedin.com/in/robin-sahin-15404a24b/'
+            className='contact__links--link'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <p>linkedin</p>
           </a>
 
           <a
-            href="https://github.com/r0binsahin"
-            className="contact__links--link"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://github.com/r0binsahin'
+            className='contact__links--link'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <p>github</p>
           </a>
         </div>
-        <div className="contact__copy">
+        <div className='contact__copy'>
           <p>&copy; 2024</p>
         </div>
       </div>
-      <div className="fullName">
+      <div className='fullName'>
+        <span>Developed by</span>
         <p>robin sahin</p>
       </div>
     </div>
