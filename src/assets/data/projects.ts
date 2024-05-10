1e9e55402cf8520794824f3fee5d976567023996
r0binsahin <robin.sahin88@gmail.com>
2024-05-10 10:53:32 +0200

    (fix): dropbox image url

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -15,13 +15,13 @@ export const projects: IProject[] = [
     github: '',
     live: '',
     isLive: false,
-    img1: 'https://www.dl.dropboxusercontent.com/scl/fi/ro0dzpnue2jjlb7aktq8k/nano1.png?rlkey=o0nbaxrorezwnjs6kjnfc5kvr&st=bcaa692u&dl=0',
-    img2: 'https://www.dl.dropboxusercontent.com/scl/fi/2jiiuhhqlvfhhbgtp8i0q/nano2.png?rlkey=m3gv51txftjy93zq4lss3461a&st=eyahibrw&dl=0',
-    img3: 'https://www.dl.dropboxusercontent.com/scl/fi/70in4yak2ak08z8xuzxpx/nano3.png?rlkey=r2jmuikgknz3du0blscktyrse&st=mnp8wk5z&dl=0',
+    img1: 'https://www.dropbox.com/scl/fi/ro0dzpnue2jjlb7aktq8k/nano1.png?rlkey=o0nbaxrorezwnjs6kjnfc5kvr&st=zjouyqre&raw=1',
+    img2: 'https://www.dropbox.com/scl/fi/2jiiuhhqlvfhhbgtp8i0q/nano2.png?rlkey=m3gv51txftjy93zq4lss3461a&st=eyahibrw&raw=1',
+    img3: 'https://www.dropbox.com/scl/fi/70in4yak2ak08z8xuzxpx/nano3.png?rlkey=r2jmuikgknz3du0blscktyrse&st=mnp8wk5z&raw=1',
     imgExtra:
-      'https://www.dl.dropboxusercontent.com/scl/fi/60999qpn49fp1rk6qdn7s/nanoExtra.jpg?rlkey=hk2hbg5erii3kvgrgbw0bfuzp&st=zxrzv7wq&dl=0',
+      'https://www.dropbox.com/scl/fi/60999qpn49fp1rk6qdn7s/nanoExtra.jpg?rlkey=hk2hbg5erii3kvgrgbw0bfuzp&st=zxrzv7wq&raw=1',
     coverImg:
-      'https://www.dl.dropboxusercontent.com/scl/fi/fjpdh3ckc0pd3eom6u94l/nanoCover.png?rlkey=jl1gn5ctd3uvu84ii2p3cfayy&st=qapknc0s&dl=0',
+      'https://www.dropbox.com/scl/fi/fjpdh3ckc0pd3eom6u94l/nanoCover.png?rlkey=jl1gn5ctd3uvu84ii2p3cfayy&st=qapknc0s&raw=1',
     isProtected: false,
   },
 
@@ -40,13 +40,13 @@ export const projects: IProject[] = [
     github: '',
     live: 'https://checkin.daresay.io/',
     isLive: true,
-    img1: 'https://www.dl.dropboxusercontent.com/scl/fi/pra3xhrp6dopkuqb68rzr/checkin1.png?rlkey=e4502r0irwhrnrlod2iuj7rgr&st=istwgug7&dl=0',
-    img2: 'https://www.dl.dropboxusercontent.com/scl/fi/4gwob4hvp9dc8bshuag6l/checkin2.png?rlkey=iqrc4eoayb6plxlzr6fta2hfw&st=kn0d2t7f&dl=0',
-    img3: 'https://www.dl.dropboxusercontent.com/scl/fi/t2fix1kmcth6uom4sxf88/checkin3.png?rlkey=fkowc1nass2mxx3xokgnnxdn6&st=j0cfdebl&dl=0',
+    img1: 'https://www.dropbox.com/scl/fi/pra3xhrp6dopkuqb68rzr/checkin1.png?rlkey=e4502r0irwhrnrlod2iuj7rgr&st=istwgug7&raw=1',
+    img2: 'https://www.dropbox.com/scl/fi/4gwob4hvp9dc8bshuag6l/checkin2.png?rlkey=iqrc4eoayb6plxlzr6fta2hfw&st=kn0d2t7f&raw=1',
+    img3: 'https://www.dropbox.com/scl/fi/t2fix1kmcth6uom4sxf88/checkin3.png?rlkey=fkowc1nass2mxx3xokgnnxdn6&st=j0cfdebl&raw=1',
     imgExtra:
-      'https://www.dl.dropboxusercontent.com/scl/fi/g6nxq9l9iq9os547pzpab/checkinExtra.jpg?rlkey=guyw6mwgwvcfgqr8atxbivfhj&st=ptbn32j3&dl=0',
+      'https://www.dropbox.com/scl/fi/g6nxq9l9iq9os547pzpab/checkinExtra.jpg?rlkey=guyw6mwgwvcfgqr8atxbivfhj&st=ptbn32j3&raw=1',
     coverImg:
-      'https://www.dl.dropboxusercontent.com/scl/fi/jdhz0gl7fx9t0valqljz2/checkinCover.png?rlkey=g5l0w47ijbli4wlv90q1nb2cv&st=hhoxag3i&dl=0',
+      'https://www.dropbox.com/scl/fi/jdhz0gl7fx9t0valqljz2/checkinCover.png?rlkey=g5l0w47ijbli4wlv90q1nb2cv&st=hhoxag3i&raw=1',
     isProtected: false,
   },
 
@@ -64,13 +64,13 @@ export const projects: IProject[] = [
     github: '',
     live: '',
     isLive: false,
-    img1: 'https://www.dl.dropboxusercontent.com/scl/fi/ri9bivo1snxqbbi5jiwld/alligo1.png?rlkey=9fig1nt90xygqxguzuc6ccp15&st=htee51rf&dl=0',
-    img2: 'https://www.dl.dropboxusercontent.com/scl/fi/vasjcpxg4zgvfgwb6k8ta/alligo2.png?rlkey=9k1qea8oteewjlpafh5l92yj5&st=vjoouexu&dl=0',
-    img3: 'https://www.dl.dropboxusercontent.com/scl/fi/2g6cywg7s6i61kkih0o7m/alligo3.png?rlkey=jpjdtgotpnhrsh51zn9me8sob&st=g5avwkjf&dl=0',
+    img1: 'https://www.dropbox.com/scl/fi/ri9bivo1snxqbbi5jiwld/alligo1.png?rlkey=9fig1nt90xygqxguzuc6ccp15&st=htee51rf&raw=1',
+    img2: 'https://www.dropbox.com/scl/fi/vasjcpxg4zgvfgwb6k8ta/alligo2.png?rlkey=9k1qea8oteewjlpafh5l92yj5&st=vjoouexu&raw=1',
+    img3: 'https://www.dropbox.com/scl/fi/2g6cywg7s6i61kkih0o7m/alligo3.png?rlkey=jpjdtgotpnhrsh51zn9me8sob&st=g5avwkjf&raw=1',
     imgExtra:
-      'https://www.dl.dropboxusercontent.com/scl/fi/1yd15o2gnalhaihlsc8gq/alligoExtra.jpg?rlkey=ikiv1hgtg6aanivv299has30e&st=20qppi4c&dl=0',
+      'https://www.dropbox.com/scl/fi/1yd15o2gnalhaihlsc8gq/alligoExtra.jpg?rlkey=ikiv1hgtg6aanivv299has30e&st=20qppi4c&raw=1',
     coverImg:
-      'https://www.dl.dropboxusercontent.com/scl/fi/int6rvshp291ymuzqshyw/alligoCover.png?rlkey=8ld1ouhoqtw4trn3isspckzk0&st=8ndlh69x&dl=0',
+      'https://www.dropbox.com/scl/fi/int6rvshp291ymuzqshyw/alligoCover.png?rlkey=8ld1ouhoqtw4trn3isspckzk0&st=8ndlh69x&raw=1',
     isProtected: false,
   },
 
@@ -88,13 +88,13 @@ export const projects: IProject[] = [
     github: '',
     live: '',
     isLive: false,
-    img1: 'https://www.dl.dropboxusercontent.com/scl/fi/qaa111evc0c569ujkk5iw/alisa1.png?rlkey=pfyjuc3q76hhx5phsgjwthyyt&st=py8tzpld&dl=0',
-    img2: 'https://www.dl.dropboxusercontent.com/scl/fi/1jd6tgcnvsu3bewgpejsl/alisa2.png?rlkey=r8ev5lpd83zwnlepvc7jmbapt&st=7upi51rx&dl=0',
-    img3: 'https://www.dl.dropboxusercontent.com/scl/fi/pn47qw3vxhxaobkec8dz8/alisa3.png?rlkey=5ibxlql5mh75686gr5vayduw4&st=o5e8v614&dl=0',
+    img1: 'https://www.dropbox.com/scl/fi/qaa111evc0c569ujkk5iw/alisa1.png?rlkey=pfyjuc3q76hhx5phsgjwthyyt&st=py8tzpld&raw=1',
+    img2: 'https://www.dropbox.com/scl/fi/1jd6tgcnvsu3bewgpejsl/alisa2.png?rlkey=r8ev5lpd83zwnlepvc7jmbapt&st=7upi51rx&raw=1',
+    img3: 'https://www.dropbox.com/scl/fi/pn47qw3vxhxaobkec8dz8/alisa3.png?rlkey=5ibxlql5mh75686gr5vayduw4&st=o5e8v614&raw=1',
     imgExtra:
-      'https://www.dl.dropboxusercontent.com/scl/fi/hn62qerporc2pk3a9uu0o/alisaExtra.png?rlkey=602t0qrrtvhs00adsrrk0qjkk&st=lclsiupu&dl=0',
+      'https://www.dropbox.com/scl/fi/hn62qerporc2pk3a9uu0o/alisaExtra.png?rlkey=602t0qrrtvhs00adsrrk0qjkk&st=lclsiupu&raw=1',
     coverImg:
-      'https://www.dl.dropboxusercontent.com/scl/fi/jzungocnsglmcnkzyyklk/alisaCover.png?rlkey=uqczbazw0wa1k68cg6gez31bh&st=ugzgk008&dl=0',
+      'https://www.dropbox.com/scl/fi/jzungocnsglmcnkzyyklk/alisaCover.png?rlkey=uqczbazw0wa1k68cg6gez31bh&st=ugzgk008&raw=1',
     isProtected: false,
   },
 
@@ -113,13 +113,13 @@ export const projects: IProject[] = [
     github: 'https://github.com/r0binsahin/Peyvok',
     live: '',
     isLive: false,
-    img1: 'https://www.dl.dropboxusercontent.com/scl/fi/xub5c3ojuvvl4lw2d9yty/peyvok1_new.png?rlkey=2oqe043lx0tf1qxr8vdltqv1e&st=p48lxm3f&dl=0',
-    img2: 'https://www.dl.dropboxusercontent.com/scl/fi/h2mwlnq68kcyzxbx6emnx/Peyvok2.png?rlkey=xnyinprq2t5cisbwu9l20ixg6&st=dv6hup1k&dl=0',
-    img3: 'https://www.dl.dropboxusercontent.com/scl/fi/b2o1v12ybqwbdn8k52grs/peyvok3_new.png?rlkey=xb9ir6xeqe9m8e1l2z1j7j0gk&st=wa0v2701&dl=0',
+    img1: 'https://www.dropbox.com/scl/fi/xub5c3ojuvvl4lw2d9yty/peyvok1_new.png?rlkey=2oqe043lx0tf1qxr8vdltqv1e&st=p48lxm3f&raw=1',
+    img2: 'https://www.dropbox.com/scl/fi/h2mwlnq68kcyzxbx6emnx/Peyvok2.png?rlkey=xnyinprq2t5cisbwu9l20ixg6&st=dv6hup1k&raw=1',
+    img3: 'https://www.dropbox.com/scl/fi/b2o1v12ybqwbdn8k52grs/peyvok3_new.png?rlkey=xb9ir6xeqe9m8e1l2z1j7j0gk&st=wa0v2701&raw=1',
     imgExtra:
-      'https://www.dl.dropboxusercontent.com/scl/fi/wbiw1xkz6fcdv71m7y8v3/peyvokExtra.png?rlkey=mpwo0zurqgndh01mfr6c13aan&st=xj1vnof2&dl=0',
+      'https://www.dropbox.com/scl/fi/wbiw1xkz6fcdv71m7y8v3/peyvokExtra.png?rlkey=mpwo0zurqgndh01mfr6c13aan&st=xj1vnof2&raw=1',
     coverImg:
-      "https://www.dl.dropboxusercontent.com/scl/fi/judyytbg8hbqk5qi158c7/peyvokCover.png?rlkey=5qfv2rys03bp2330ki6rf0h1t&st=fqls238r&dl=0'",
+      'https://www.dropbox.com/scl/fi/judyytbg8hbqk5qi158c7/peyvokCover.png?rlkey=5qfv2rys03bp2330ki6rf0h1t&st=ws2elg3h&raw=1',
     isProtected: false,
   },
 
@@ -138,13 +138,13 @@ export const projects: IProject[] = [
     github: '',
     live: '',
     isLive: false,
-    img1: 'https://www.dl.dropboxusercontent.com/scl/fi/cq134ktoxp7dtch4z8wf3/stv1.png?rlkey=7wehmlo07v5qgrm5gs8km0oi8&st=cq4soc2m&dl=0',
-    img2: 'https://www.dl.dropboxusercontent.com/scl/fi/xsprpsw94jnkwe4dnmd7h/stv2.png?rlkey=h78w7htvfi5gp09tir7vzr2qs&st=sehcxuvg&dl=0',
-    img3: 'https://www.dl.dropboxusercontent.com/scl/fi/8kpvpqy2813arazych4ja/stv3.png?rlkey=5j6wxh7wn87ahl2s8twct4h39&st=kfpdejpb&dl=0',
+    img1: 'https://www.dropbox.com/scl/fi/cq134ktoxp7dtch4z8wf3/stv1.png?rlkey=7wehmlo07v5qgrm5gs8km0oi8&st=cq4soc2m&raw=1',
+    img2: 'https://www.dropbox.com/scl/fi/xsprpsw94jnkwe4dnmd7h/stv2.png?rlkey=h78w7htvfi5gp09tir7vzr2qs&st=sehcxuvg&raw=1',
+    img3: 'https://www.dropbox.com/scl/fi/8kpvpqy2813arazych4ja/stv3.png?rlkey=5j6wxh7wn87ahl2s8twct4h39&st=kfpdejpb&raw=1',
     imgExtra:
-      'https://www.dl.dropboxusercontent.com/scl/fi/zjqrxmjpvc020qiibdbwl/stvExtra.png?rlkey=gqilqgig66ydy65hqberu3lvx&st=8fif2ypp&dl=0',
+      'https://www.dropbox.com/scl/fi/zjqrxmjpvc020qiibdbwl/stvExtra.png?rlkey=gqilqgig66ydy65hqberu3lvx&st=8fif2ypp&raw=1',
     coverImg:
-      'https://www.dl.dropboxusercontent.com/scl/fi/n9ij91trbbuebqau51svb/stvCover.png?rlkey=j0xbugkjwaftve6mb5xzlmb85&st=iv5ed6no&dl=0',
+      'https://www.dropbox.com/scl/fi/n9ij91trbbuebqau51svb/stvCover.png?rlkey=j0xbugkjwaftve6mb5xzlmb85&st=iv5ed6no&raw=1',
     isProtected: true,
   },
 ];
