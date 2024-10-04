9332d78a07bba713714104b91f448856ddaf4e81
r0binsahin <robin.sahin88@gmail.com>
2024-10-04 14:44:27 +0200

    adds intro image

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,19 +1,24 @@
-import "./intro.scss";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
+import './intro.scss';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import portrait from '../../assets/images/robin_sahin_round.jpeg';
 
 const Intro = () => {
   useGSAP(() => {
     const tlIntro = gsap.timeline({});
 
-    tlIntro.from(".firstname", { duration: 2, x: 1600 });
-    tlIntro.from(".developer", { duration: 0.8, y: -100 });
+    tlIntro.from('.firstname', { duration: 2, x: 1600 });
+    /*     tlIntro.from('.developer', { duration: 0.8, y: -100 }); */
+    tlIntro.from('.wrapper__portrait', { duration: 1.5, y: -500 });
   }, []);
   return (
-    <div className="wrapper">
-      <div>
-        <h3 className="developer">Developer</h3>
-        <h1 className="firstname">Robin</h1>
+    <div className='wrapper'>
+      <div className='wrapper__portrait'>
+        <img src={portrait} alt='portrait of Robin Sahin' />
+      </div>
+      <div className='wrapper__titles'>
+        <h3 className='developer'>Developer</h3>
+        <h1 className='firstname'>Robin</h1>
       </div>
     </div>
   );
